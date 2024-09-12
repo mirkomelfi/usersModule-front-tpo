da227a56d57dffbfbea056f1e54376a94a2aa76d
Marianofb <maonafb@gmail.com>
2024-09-12 13:11:37 -0300

    Productos_MiniUpdate

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Productos.css';
 import "../Global.css";
 
+import foto from './Ojotas.jpg';
+
 const productos = [
-  { id: 1, name: 'Crocs Crocband Clog', price: '$40.862', imageUrl: 'link-to-image', tipo: 'Calzado' },
-  { id: 2, name: 'Crocs Classic Originales', price: '$32.990', imageUrl: 'link-to-image', tipo: 'Calzado' },
-  { id: 3, name: 'Sandalias Mujer Taco Bajo', price: '$24.900', imageUrl: 'link-to-image', tipo: 'Sandalias' },
-  { id: 4, name: 'Ojotas Adidas Shower', price: '$39.999', imageUrl: 'link-to-image', tipo: 'Ojotas' },
-  { id: 5, name: 'Crocs Azul Unisex', price: '$44.990', imageUrl: 'link-to-image', tipo: 'Calzado' },
+  { id: 1, name: 'Crocs Crocband Clog', price: '$40.862', image: foto, tipo: 'Botines' },
+  { id: 2, name: 'Crocs Classic Originales', price: '$32.990', image: foto, tipo: 'Calzado' },
+  { id: 3, name: 'Sandalias Mujer Taco Bajo', price: '$24.900', image: foto, tipo: 'Sandalias' },
+  { id: 4, name: 'Ojotas Adidas Shower', price: '$39.999', image: foto, tipo: 'Botines' },
+  { id: 5, name: 'Crocs Azul Unisex', price: '$44.990', image: foto, tipo: 'Calzado' },
   // Agrega más productos según necesites
 ];
 
@@ -37,7 +39,7 @@ export const Productos = () => {
           <div className="productos-grid">
             {productosPorTipo[tipo].map(producto => (
               <div key={producto.id} className="producto-card">
-                <img src={producto.imageUrl} alt={producto.name} className="producto-image" />
+                <img src={producto.image} alt={producto.name} className="producto-image" />
                 <div className="producto-info">
                   <p className="producto-name">{producto.name}</p>
                   <p className="producto-price">{producto.price}</p>
