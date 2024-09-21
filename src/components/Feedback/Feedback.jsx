549b4952322c55edc99107afb0f1394e080ae908
mirkomelfi <mirko_melfi_02@hotmail.com>
2024-09-20 22:11:04 -0300

    feedbacksfetchOK

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,10 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feedback.css';
 
 export const Feedback = () => {
-  const [selectedSubject, setSelectedSubject] = useState('');
+
+const dni=111
+
+  const [selectedRubro, setSelectedRubro] = useState('');
   const [comment, setComment] = useState('');
 
+
+  const [listaRubros,setListaRubros]= useState([]);
+  const [mensaje,setMensaje]= useState(null);
+
+  const ejecutarFetch = async() =>{
+
+    const response= await fetch(`${process.env.REACT_APP_DOMINIO_BACK}/rubros`, {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json",
+          // "Authorization": `Bearer ${getToken()}`
+      }
+      
+    })
+
+    const data = await response.json()
+    console.log(data)
+    if (data.msj){
+      setMensaje(data.msj)
+    }else{
+      setListaRubros(data)
+    }
+  }
+
+  const enviarFeedback = async(feedback) =>{
+
+    const feedbackObject={descripcion:feedback}
+
+    const response= await fetch(`${process.env.REACT_APP_DOMINIO_BACK}/feedbacks/${dni}/rubro/${selectedRubro}`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          // "Authorization": `Bearer ${getToken()}`
+      },
+      body:JSON.stringify(feedbackObject)
+      
+    })
+
+    const data = await response.json()
+    console.log(data)
+    if (data.msj){
+      setMensaje(data.msj)
+    }else{
+      setListaRubros(data)
+    }
+  }
+
+  useEffect(() => { 
+    ejecutarFetch()
+    .catch(error => console.error(error))
+
+  },[])
+  useEffect(() => { 
+   console.log(selectedRubro)
+
+  },[selectedRubro])
+
+/*
   const subjects = [
     'Sugerencias para el club',
     'Problemas con el pago',
@@ -17,15 +78,21 @@ export const Feedback = () => {
     'Soporte técnico en la web',
     'Otros temas',
   ];
+*/
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Aquí podrías manejar el envío del feedback (como enviarlo a un backend)
-    console.log('Asunto:', selectedSubject);
+    console.log('Asunto:', selectedRubro);
     console.log('Comentario:', comment);
+
+    enviarFeedback(comment)
+
     // Reseteamos el formulario
-    setSelectedSubject('');
+    setSelectedRubro('');
     setComment('');
+
+
   };
 
   return (
@@ -35,16 +102,16 @@ export const Feedback = () => {
         <label htmlFor="subject">Selecciona el asunto:</label>
         <select
           id="subject"
-          value={selectedSubject}
-          onChange={(e) => setSelectedSubject(e.target.value)}
+          value={selectedRubro}
+          onChange={(e) => setSelectedRubro(e.target.value)}
           required
         >
           <option value="" disabled>
             Elige un asunto
           </option>
-          {subjects.map((subject, index) => (
-            <option key={index} value={subject}>
-              {subject}
+          {listaRubros.map((subject, index) => (
+            <option key={index} value={subject.idRubro}>
+              {subject.descripcion}
             </option>
           ))}
         </select>
